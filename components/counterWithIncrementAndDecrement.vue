3c0efc060165c3b4e2d1bfd66e7f2e4b96c4f1ba
iacobucci <iacobuccivalerio@gmail.com>
2025-02-22 18:38:43 +0100

    working shadcn cli

--- components/counterWithIncrementAndDecrement.vue
+++ components/counterWithIncrementAndDecrement.vue
@@ -7,8 +7,8 @@ const { count, increment, decrement } = useCounter();
 	<div>
 		<p>Component: counterWithIncrement</p>
 		<p>Count: {{ count }}</p>
-		<button @click="increment">Increment</button>
-		<button @click="decrement">Decrement</button>
+		<Button @click="increment">Increment</Button>
+		<Button @click="decrement">Decrement</Button>
 	</div>
 </template>
 

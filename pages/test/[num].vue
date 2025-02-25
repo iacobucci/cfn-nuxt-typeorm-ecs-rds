40a08b76f7835b2bb57a3a57888a04778eda20c3
iacobucci <iacobuccivalerio@gmail.com>
2025-02-25 09:58:21 +0100

    new file:   pages/test/[num].vue

---
+++ pages/test/[num].vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+definePageMeta({
+	layout: "empty"
+})
+
+const route = useRoute()
+
+console.log(route.params.num)
+
+</script>
+
+<template>
+	<div class="min-h-screen flex flex-col items-center justify-center">
+		<p>{{ $route.params.num }}</p>
+	</div>
+</template>
